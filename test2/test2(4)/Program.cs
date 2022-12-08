f25dce98bb0f0b6405fe5d106ab5c1abcc3138df
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-08 19:42:05 +0900

    2차실기

--- test2/test2(4)/Program.cs
+++ test2/test2(4)/Program.cs
@@ -16,7 +16,7 @@ namespace test2_4_
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); //Form1생성
         }
     }
 }
