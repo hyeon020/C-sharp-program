82a9daacb11f0d37c9bb786b5be9c5919cd0534f
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-08 21:21:55 +0900

    chapter4 예제1

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
-    internal class Program
+    class Fraction
     {
-        static void Main(string[] args)
+        int numerator;
+        int denominator;
+        public Fraction(int num, int denom)
         {
+            numerator = num;
+            denominator = denom;
+        }
+        public void PrintFraction() {
+            Console.WriteLine(numerator + "/" + denominator);
+        }
+        class FractionApp{
+            public static void Main() {
+                Fraction f = new Fraction(1,2);
+                f.printFraction();
+            }
         }
     }
 }
