d8c55b463f35303acfdfea12b548848091770af0
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-08 23:25:31 +0900

    chapter2

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 9, y = 3;
+            Console.WriteLine(x + " & " + y + " = " + (x & y));
+            Console.WriteLine(x + "     " + y + " = " + (x | y));
+            Console.WriteLine(x + " ^ " + y + " = " + (x ^ y));
+            Console.WriteLine("~10 = " + (~10));
+        }
+    }
+}
