4760f582ca55337a0367f8ea8bc24de59cffce75
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-15 18:45:28 +0900

    예제 6-24

--- chapter6/chapter6-24/Program.cs
+++ chapter6/chapter6-24/Program.cs
@@ -14,7 +14,7 @@ namespace chapter6_24
             for(int i = 1; i <= 3; i++)
             {
                 Console.WriteLine("{0} is activated => {1}", myself.Name, i);
-                Thread.Sleep(1000);
+                Thread.Sleep(1000); //1초 
             }
         }
     }
