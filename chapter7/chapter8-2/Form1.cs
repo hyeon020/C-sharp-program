d81d671323f70eefb0d76dfef0a6533f4b2a31e2
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-17 17:35:25 +0900

    chapter8-2

--- chapter7/chapter8-2/Form1.cs
+++ chapter7/chapter8-2/Form1.cs
@@ -32,22 +32,22 @@ namespace chapter8_2
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateLabel(checkBox1.Text, checkBox1.Checked);
+            UpdateLabel(checkBox1.Text, checkBox1.Checked); //체크박스1 선택시
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateLabel(checkBox2.Text, checkBox2.Checked);
+            UpdateLabel(checkBox2.Text, checkBox2.Checked);  //체크박스2 선택시
         }
 
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateLabel(checkBox3.Text, checkBox3.Checked);
+            UpdateLabel(checkBox3.Text, checkBox3.Checked);  //체크박스3 선택시
         }
 
         private void checkBox4_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateLabel(checkBox4.Text, checkBox4.Checked);
+            UpdateLabel(checkBox4.Text, checkBox4.Checked);  //체크박스4 선택시
         }
     }
 }
